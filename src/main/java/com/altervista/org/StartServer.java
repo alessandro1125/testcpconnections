9fb5f3bed9c5c6e1ee34ddbed678db4d042bbc30
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-05-26 18:56:57 +0200

    1856

--- src/main/java/com/altervista/org/StartServer.java
+++ src/main/java/com/altervista/org/StartServer.java
@@ -1,29 +1,54 @@
 package com.altervista.org;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 @WebServlet(
         name = "HomeServlet",
         urlPatterns = {"/start_server/"}
 )
-public class Home extends HttpServlet {
+public class StartServer extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
 
+        ServerSocket welcomeSocket = null;
+        assert welcomeSocket != null;
+        new Thread(() -> {
+            while (true){
+                String clientSentence;
+                String capitalizedSentence = "ciao";
+                try {
+                    Socket connectionSocket = welcomeSocket.accept();
+                    BufferedReader inFromClient =
+                            new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                    clientSentence = inFromClient.readLine();
+                    System.out.println("Received: " + clientSentence);
+                    //capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                    outToClient.writeBytes(capitalizedSentence);
+                    //connectionSocket.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
 
-
+        /*
         try {
             RequestDispatcher view;
             view = req.getRequestDispatcher("index_main.jsp");
             view.forward(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
