00ecc6c034840acd1723f06b9c9f95626dc924a4
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-05-26 18:20:49 +0200

    1820

--- src/main/java/com/altervista/org/Home.java
+++ src/main/java/com/altervista/org/Home.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @WebServlet(
         name = "HomeServlet",
@@ -16,6 +20,19 @@ public class Home extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
 
+        try {
+            BufferedReader stream = req.getReader();
+
+            String line = stream.readLine();
+            String result = "";
+            while (line != null){
+                result.concat(line);
+                line = stream.readLine();
+            }
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         try {
@@ -31,5 +48,4 @@ public class Home extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
         doGet(req, resp);
     }
-
 }
