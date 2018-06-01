b898831d834bfd9b82f8e57b978fed99c48fef9c
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-06-01 18:47:36 +0200

    1847

--- src/main/java/com/altervista/org/Home.java
+++ src/main/java/com/altervista/org/Home.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 @WebServlet(
         name = "HomeServlet",
@@ -29,22 +31,33 @@ public class Home extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
 
-        doGet(req, resp);
-        /*
-        while (true) {
-            try {
-                BufferedReader stream = req.getReader();
+        BufferedReader stream = null;
 
-                String line = stream.readLine();
-                String result = "";
-                while (line != null) {
-                    result.concat(line);
-                    line = stream.readLine();
+        try {
+            stream = req.getReader();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            while (stream != null) {
+                try {
+                    String line = stream.readLine();
+                    String result = "";
+                    while (line != null) {
+                        result = result.concat(line);
+                        line = stream.readLine();
+                    }
+                    System.out.println(result);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.println(result);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+                try{
+                    stream = req.getReader();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
-        }*/
+        }
     }
 }
