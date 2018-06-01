95b2e588b82add4ad7f13aab8eb0c95e5196f2eb
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-06-01 18:34:01 +0200

    1833

--- src/main/java/com/altervista/org/StartServer.java
+++ src/main/java/com/altervista/org/StartServer.java
@@ -19,60 +19,47 @@ public class StartServer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
 
-        ServerSocket welcomeSocket = null;
-        try {
-            welcomeSocket = new ServerSocket(8081);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert welcomeSocket != null;
-        String clientSentence;
-        String capitalizedSentence = "ciao";
-        try {
-            Socket connectionSocket = welcomeSocket.accept();
-            while (true){
-                BufferedReader inFromClient =
-                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                clientSentence = inFromClient.readLine();
-                if (clientSentence != null && clientSentence != "")
-                    System.out.println("Received: " + clientSentence);
-                //capitalizedSentence = clientSentence.toUpperCase() + '\n';
-                //outToClient.writeBytes(capitalizedSentence);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
 
-        /*
         try {
             RequestDispatcher view;
             view = req.getRequestDispatcher("index_main.jsp");
             view.forward(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
 
-        doGet(req, resp);
-        /*
-        while (true) {
-            try {
-                BufferedReader stream = req.getReader();
+        BufferedReader stream = null;
 
-                String line = stream.readLine();
-                String result = "";
-                while (line != null) {
-                    result.concat(line);
-                    line = stream.readLine();
+        try {
+            stream = req.getReader();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            while (stream != null) {
+                try {
+                    String line = stream.readLine();
+                    String result = "";
+                    while (line != null) {
+                        result = result.concat(line);
+                        line = stream.readLine();
+                    }
+                    System.out.println(result);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.println(result);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+                try{
+                    stream = req.getReader();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
-        }*/
+        }
+
     }
 }
